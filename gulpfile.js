55453531a687837c4dda549002ff7678a237fb2e
Jimmy <jimmy.leu@myflyfit.com>
2016-01-19 15:56:51 +0800

    add gulp-compass

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ var gutil= require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
+var compass = require('gulp-compass');
 
 gulp.task('log',function(){
 	gutil.log('workflows are awesome');
 });
 
 var coffeeSources=['components/coffee/*.coffee'];
+var sassSources = ['components/sass/style.scss'];
 
 
 var jsSources = [
@@ -29,5 +31,17 @@ gulp.task('coffee',function(){
 gulp.task('js',function(){
 	gulp.src(jsSources)
 		.pipe(concat('script.js').on('error',gutil.log))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass',function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass:'components/sass',
+			image:'builds/development/images',
+			sytle:'expanded'
+		}))
+		.on('error',gutil.log)
+		.pipe(gulp.dest('builds/development/css'))
 });
