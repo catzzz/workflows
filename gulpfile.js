fbebbf43f9399d5efeaadb1258b1b0b34fcf9ee6
Jimmy <jimmy.leu@myflyfit.com>
2016-01-19 15:22:32 +0800

    add gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp=require('gulp');
 var gutil= require('gulp-util');
 var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
 
 gulp.task('log',function(){
 	gutil.log('workflows are awesome');
@@ -8,10 +9,24 @@ gulp.task('log',function(){
 
 var coffeeSources=['components/coffee/*.coffee'];
 
+
+var jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
+];
+
 gulp.task('coffee',function(){
 	gulp.src(coffeeSources)
 	.pipe(coffee({bare:true})
 		.on('error',gutil.log))
 		.pipe(gulp.dest('components/scripts'))
 		
+});
+
+gulp.task('js',function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js').on('error',gutil.log))
+		.pipe(gulp.dest('builds/development/js'))
 });
