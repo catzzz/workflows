c27a2ca4ed71c86a2d148717fb14c80e14633080
Jimmy <jimmy.leu@myflyfit.com>
2016-01-19 15:44:40 +0800

    add browserify jquery mustache

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp=require('gulp');
 var gutil= require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 
 gulp.task('log',function(){
 	gutil.log('workflows are awesome');
